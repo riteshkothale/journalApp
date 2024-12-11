46d0439028a149c9c14ec839c3e495ecbb456537
Ritesh kothale <riteshkothale@gmail.com>
2024-12-11 09:39:50 +0530

    added code for update the record

--- src/main/java/com/journal/journalApp/controller/JournalController.java
+++ src/main/java/com/journal/journalApp/controller/JournalController.java
@@ -2,9 +2,11 @@ package com.journal.journalApp.controller;
 
 import com.journal.journalApp.entity.JournalEntry;
 import com.journal.journalApp.service.JournalService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +23,7 @@ public class JournalController {
 
     @PostMapping
     public boolean createJournalEntry(@RequestBody JournalEntry journalEntry){
+        journalEntry.setDate(LocalDateTime.now());
         journalService.createJournalEntry(journalEntry);
         return true;
     }
@@ -31,18 +34,25 @@ public class JournalController {
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable Long myId){
-        return journalEntryMap.get(myId);
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
+        return journalService.findById(myId).orElse(null);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteEntryById(@PathVariable Long myId){
-
+    public boolean deleteEntryById(@PathVariable ObjectId myId){
+        journalService.deleteById(myId);
         return true;
     }
 
     @PutMapping("id/{myId}")
-    public JournalEntry updateJournalEntry(@PathVariable Long myId, @RequestBody JournalEntry journalEntry){
-        return journalEntryMap.put(myId,journalEntry);
+    public JournalEntry updateJournalEntry(@PathVariable ObjectId myId, @RequestBody JournalEntry newJournalEntry){
+
+        JournalEntry old = journalService.findById(myId).orElse(null);
+        if(old != null){
+            old.setTitle(old.getTitle() != null && newJournalEntry.getTitle().equals("") ? newJournalEntry.getTitle():old.getTitle());
+            old.setContent(old.getContent() != null && newJournalEntry.getTitle().equals("") ? newJournalEntry.getContent(): old.getContent());
+        }
+        journalService.createJournalEntry(old);
+        return old;
     }
 }
--- src/main/java/com/journal/journalApp/service/JournalService.java
+++ src/main/java/com/journal/journalApp/service/JournalService.java
@@ -2,10 +2,12 @@ package com.journal.journalApp.service;
 
 import com.journal.journalApp.entity.JournalEntry;
 import com.journal.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JournalService {
@@ -19,5 +21,13 @@ public class JournalService {
 
     public List<JournalEntry> getAll(){
         return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
     }
 }
